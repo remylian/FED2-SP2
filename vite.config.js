88e589d105e6b982cb8261444f5f6eddd065def5
Remy Lian <137064504+remylian@users.noreply.github.com>
2025-09-19 00:28:10 +0200

    added multipage rollup input in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve} from "node:path"
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  server: {
-    open: true, 
+  server: { open: true },
+  build: {
+    rollupOptions: {
+      input: {
+        index:        resolve(__dirname, "index.html"),
+        listing:      resolve(__dirname, "listing.html"),
+        login:        resolve(__dirname, "login.html"),
+        register:     resolve(__dirname, "register.html"),
+        profile:      resolve(__dirname, "profile.html"),
+        seller:       resolve(__dirname, "seller.html"),
+        createListing:resolve(__dirname, "create-listing.html"),
+        editListing:  resolve(__dirname, "edit-listing.html"),
+        editProfile:  resolve(__dirname, "edit-profile.html"),
+      },
+    },
   },
 });
